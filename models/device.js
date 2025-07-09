b40280f3ceb7a359c7753164a6e3f636937ef214
Claudiu Ghise <claudiughise@gmail.com>
2025-07-09 09:52:25 +0200

    memmert device schema

---
+++ models/device.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const deviceSchema = new mongoose.Schema({
+  serialNum: {
+    type: String,
+    required: true,
+  },
+  deviceType: {
+    type: String,
+  },
+  temp1Read: {
+    type: Number,
+    required: true,
+  },
+  temp1Set: {
+    type: Number,
+  },
+  humRead: {
+    type: Number,
+  },
+  humSet: {
+    type: Number,
+  },
+});
+
+const MemmertDevice = mongoose.model("MemmertDevice", "deviceSchema");
+module.exports = MemmertDevice;
